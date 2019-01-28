ccc4102e2d0b1009fdc5ab04121fbe74b77cccb3
François-Régis LANCIEN <francois.regis.lancien@gmail.com>
2019-01-28 23:09:41 +0100

    Added providers

--- src/app/pages/exercises/exercises.page.ts
+++ src/app/pages/exercises/exercises.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActionSheetController} from '@ionic/angular';
+import {IExercise} from '../../providers/models/exercise.model';
 
 @Component({
     selector: 'app-exercises',
@@ -7,6 +8,24 @@ import {ActionSheetController} from '@ionic/angular';
     styleUrls: ['./exercises.page.scss'],
 })
 export class ExercisesPage implements OnInit {
+
+    public exercisesData: IExercise[] = [{
+        id: '04692273-7353-4692-9f8d-23a4a7b569d0',
+        name: 'JR Normal jumps',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo dolor non eros iaculis luctus. Morbi ullamcorper justo ut turpis luctus dapibus.',
+        creationDate: '2019-01-28T23:06:47+01:00',
+        updateDate: null,
+        isFavorite: true,
+        difficulty: 'Facile',
+    }, {
+        id: '04692273-7374-4692-9f8d-23a4a7b569d0',
+        name: 'JR Double unders',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo dolor non eros iaculis luctus. Morbi ullamcorper justo ut turpis luctus dapibus.',
+        creationDate: '2019-01-27T23:06:47+01:00',
+        updateDate: '2019-01-28T23:06:47+01:00',
+        isFavorite: false,
+        difficulty: 'Difficile',
+    }];
 
     constructor(private actionSheetController: ActionSheetController) {
     }
@@ -17,33 +36,33 @@ export class ExercisesPage implements OnInit {
     async onExerciseClicked() {
         const actionSheet = await this.actionSheetController.create({
             buttons: [{
-                    text: 'Voir',
-                    handler: () => {
-                        console.log('Favorite clicked');
-                    }
-                }, {
-                    text: 'Modifier',
-                    handler: () => {
-                        console.log('Share clicked');
-                    }
-                }, {
-                    text: 'Ajouter aux favoris',
-                    handler: () => {
-                        console.log('Favorite clicked');
-                    }
-                }, {
-                    text: 'Supprimer',
-                    role: 'destructive',
-                    handler: () => {
-                        console.log('Delete clicked');
-                    }
-                }, {
-                    text: 'Annuler',
-                    role: 'cancel',
-                    handler: () => {
-                        console.log('Cancel clicked');
-                    }
-                }]
+                text: 'Voir',
+                handler: () => {
+                    console.log('Favorite clicked');
+                }
+            }, {
+                text: 'Modifier',
+                handler: () => {
+                    console.log('Share clicked');
+                }
+            }, {
+                text: 'Ajouter aux favoris',
+                handler: () => {
+                    console.log('Favorite clicked');
+                }
+            }, {
+                text: 'Supprimer',
+                role: 'destructive',
+                handler: () => {
+                    console.log('Delete clicked');
+                }
+            }, {
+                text: 'Annuler',
+                role: 'cancel',
+                handler: () => {
+                    console.log('Cancel clicked');
+                }
+            }]
         });
         await actionSheet.present();
     }
---
+++ src/app/providers/models/exercise.model.ts
@@ -0,0 +1,9 @@
+export interface IExercise {
+    id: string;
+    name: string;
+    description: string;
+    creationDate: string;
+    updateDate: string;
+    isFavorite: boolean;
+    difficulty: string;
+}
